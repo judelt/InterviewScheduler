45c1b1bf464490dbe4c1890a0cea98c561ce9cbc
judelt <mendez.puig@gmail.com>
2021-03-16 20:19:37 -0700

    create component & add logic

--- src/components/Appointment/Confirm.js
+++ src/components/Appointment/Confirm.js
@@ -6,8 +6,8 @@ export default function Confirm(props) {
     <main className="appointment__card appointment__card--confirm">
       <h1 className="text--semi-bold">{props.message}</h1>
       <section className="appointment__actions">
-        <Button danger>{props.onConfirm}</Button>
-        <Button danger>{props.onCancel}</Button>
+        <Button danger onClick={props.onConfirm}>Cancel</Button>
+        <Button confirm onClick={props.onConfirm}>Confirm</Button>
       </section>
     </main>
   );
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,11 +1,17 @@
-import React from "react";
-// import "components/Appointment/styles.scss ";
+import React, { Fragment } from "react";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
+import "./styles.scss";
 
 export default function Appointment(props) {
-  
-
   return (
-    <article className="appointment"></article>
+    <>
+      <article className="appointment">
+        <Header time={props.time} />
+        {props.interview? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />}
+      </article>
+    </>
   );
 }
