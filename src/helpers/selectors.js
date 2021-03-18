ffd6f45c94f6e58a0746d3c2d5dabdd0f3bfada6
judelt <mendez.puig@gmail.com>
2021-03-17 18:20:57 -0700

    create getAppointmentsForDay & tests for it

---
+++ src/helpers/selectors.js
@@ -0,0 +1,25 @@
+export const getAppointmentsForDay = (state, day) => {
+  const result = [];
+  const days = state.days;
+  const appointments = state.appointments;
+  let appointmentIds = [];
+  
+  //get appointments ids
+  for (const particularDay of days) {
+    if (particularDay.name === day) {
+      appointmentIds = [...particularDay.appointments]
+      break;
+    }
+  }
+  
+  //get specific appointments that match the ids
+  for (const particularDay of appointmentIds) {
+    for (const key in appointments) {
+      if (particularDay === appointments[key].id) {
+        result.push(appointments[key]);
+        break;
+      }
+    }
+  }
+  return result;
+}
