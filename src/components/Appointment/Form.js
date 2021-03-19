dceba97b7f329605e96bb2751bd335d0f1117ba5
judelt <mendez.puig@gmail.com>
2021-03-18 19:23:21 -0700

    add new logic

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -8,15 +8,15 @@ export default function Form(props) {
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
-  const reset = () => {
-    setName("");
-    setInterviewer(null);
-  }
+  // const reset = () => {
+  //   setName("");
+  //   setInterviewer(null);
+  // }
 
-  const cancel = () => {
-    reset()
-    props.onCancel()
-  }
+  // const cancel = () => {
+  //   reset()
+  //   props.onCancel()
+  // }
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -39,7 +39,8 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={cancel}>Cancel</Button> 
+          {/* <Button danger onClick={cancel}>Cancel</Button>  */}
+          <Button danger onClick={props.onCancel}>Cancel</Button> 
           <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
