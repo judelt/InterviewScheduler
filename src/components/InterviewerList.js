c55f64fdb36f9452a09108ff0185d86e6299d84a
judelt <mendez.puig@gmail.com>
2021-03-22 15:32:29 -0700

    implement propTypes

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -2,7 +2,7 @@ import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss";
 
-// import PropTypes from "prop-types";
+import PropTypes from "prop-types";
 
 export default function InterviewerList(props) {
 
@@ -27,4 +27,4 @@ export default function InterviewerList(props) {
 }
 
 
-// InterviewerList.propTypes = {  value: PropTypes.number,  onChange: PropTypes.func.isRequired,  interviewers: PropTypes.array.isRequired};
+InterviewerList.propTypes = { interviewers: PropTypes.array.isRequired};
