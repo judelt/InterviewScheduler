d1ae15ba1bc61490c365c92ba9d4aeddaad02c3d
judelt <mendez.puig@gmail.com>
2021-03-23 14:39:22 -0700

    delete console log

--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -2,7 +2,6 @@ import React from "react";
 
 export default function Show(props) {
 
-  console.log('props.interviewer', props.interviewer)
   return (
     <main className="appointment__card appointment__card--show">
       <section className="appointment__card-left">
